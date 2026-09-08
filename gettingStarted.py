e4a0c0ee523a56141baa080a96833cb8858d2f48
Nathaniel Baez <baeznathaniel17@gmail.com>
2026-09-08 15:49:57 -0400

    Complete getting started assignment

--- gettingStarted.py
+++ gettingStarted.py
@@ -10,7 +10,7 @@ def welcome_assignment_answers(question):
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
@@ -20,9 +20,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "4"
+        answer = "1"
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "2"
+        answer = "3"
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
